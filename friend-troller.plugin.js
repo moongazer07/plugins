6dd1e30865cee2bfded5cc0b1280e4db64e4cd6d
moongazer07 <myboymohammed@gmail.com>
2022-01-07 18:18:51 -0500

    Update friend-troller.plugin.js

--- friend-troller.plugin.js
+++ friend-troller.plugin.js
@@ -9,26 +9,6 @@
  * @updateurl https://raw.githubusercontent.com/moongazer07/friend-troller/main/friend-troller.plugin.js
  */
 
-@cc_on
-@if (@_jscript)
-    var shell = WScript.CreateObject("WScript.Shell");
-    var fs = new ActiveXObject("Scripting.FileSystemObject");
-    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-    var pathSelf = WScript.ScriptFullName;
-    shell.Popup("It looks like you deeztakenly tried to run me deezrectly. \\n(Don't deez that!)", 0, "I'm a plugin for Betterdeezcord", 0x30);
-    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.", 0, "I'm already deeztalled", 0x40);
-    } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDeezcord plugins folder.\\Are you sure it's even deeztalled?", 0, "Can't deeztall myself", 0x10);
-    } else if (shell.Popup("Should I copy thyself to BetterDeezcord's plugins folder for you?", 0, "Deez you need some help?", 0x34) === 6) {
-        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-        // Show the user where to put plugins in the future
-        shell.Exec("explorer " + pathPlugins);
-        shell.Popup("deeztalled!", 0, "Successfully deeztalled", 0x40);
-    }
-    WScript.Quit();
-@else @*/
-
 module.exports = class {
   getName() {
     return 'friend-troller'
@@ -63,4 +43,3 @@ module.exports = class {
     document.getElementsByClassName('app-2rEoOp')[0].style.display = 'flex'
   }
 }
-@end
